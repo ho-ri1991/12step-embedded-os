e383b87e13b871cf733cb9dee029a3ef00a5338d
ho-ri1991 <hage.1991@hotmail.co.jp>
2018-10-07 00:20:00 +0900

    6th step

--- src/bootload/main.c
+++ src/bootload/main.c
@@ -58,6 +58,8 @@ int main(void)
   static long size = -1;
   static unsigned char* loadbuf = NULL;
   extern int buffer_start; // defined in linker script
+  char* entry_point;
+  void (*f)(void);
 
   init();
 
@@ -87,7 +89,19 @@ int main(void)
     }
     else if (!strcmp(buf, "run"))
     {
-      elf_load(loadbuf);
+      entry_point = elf_load(loadbuf);
+      if (!entry_point)
+      {
+        puts("run error!");
+      }
+      else
+      {
+        puts("starting from entry point: ");
+        putxval((unsigned long)entry_point, 0);
+        puts("\n");
+        f = (void(*)(void))entry_point;
+        f();
+      }
     }
     else
     {
---
+++ src/os/serial.h
@@ -0,0 +1,10 @@
+#ifndef _SERIAL_H_INCLUDED_
+#define _SERIAL_H_INCLUDED_
+
+int serial_init(int index);
+int serial_is_send_enable(int index);
+int serial_send_byte(int index, unsigned char b);
+int serial_is_recv_enable(int index);
+unsigned char serial_recv_byte(int index);
+
+#endif
